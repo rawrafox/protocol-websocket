cc8b6d4ce43d2176da0868d729ed64772429065d
Aurora <aurora@aventine.se>
2021-06-07 12:46:17 +0200

    Implement enough to make this pass autobahn-testsuite
    This adds features and fixes enough old ones that all tests in https://github.com/crossbario/autobahn-testsuite passes, even if some of them do so non-strictly.

--- gems.rb
+++ gems.rb
@@ -7,3 +7,8 @@ group :maintenance, optional: true do
 	gem "bake-bundler"
 	gem "bake-modernize"
 end
+
+group :autobahn_tests, optional: true do
+	gem "async-websocket", github: "socketry/async-websocket", ref: "b966d76fc23a31904d1432ffd88ed771f46441a3"
+	gem "falcon"
+end
